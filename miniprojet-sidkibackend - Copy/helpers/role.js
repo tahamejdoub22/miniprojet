10b0220a12cd756bf431869806e7e64f41e36a55
tahamejdoub22 <mohamedtaha.mejdoub@esprit.tn>
2022-11-26 15:52:16 +0100

    taha

--- miniprojet-sidkibackend - Copy/helpers/role.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  Admin: "Admin",
-  User: "User",
-  Entreprise:"entreprise",
-};
--- miniprojet-sidkibackend/helpers/login_schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('@hapi/joi')
-const loginSchema = Joi.object({
-    email:Joi.string().email().lowercase().required(),
-    password:Joi.string().min(2).required()
-})
-module.exports={
-    loginSchema
-}
